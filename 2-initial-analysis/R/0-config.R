16aa14c77fcc4e80940ad6a0918825e2ea846a69
pearl-antetestard <pearl-anne.ante@lecnam.net>
2022-10-17 07:48:02 -0700

    Configuration

---
+++ 2-initial-analysis/R/0-config.R
@@ -0,0 +1,30 @@
+
+#-------------------------------------------------------------------------------
+# @Organization - Proctor Foundation - UCSF
+# @Project - WASHB-SEP-Season
+# @Author - Pearl Ante-Testard, pearl.ante@ucsf.edu
+# @Description - Configuration
+#-------------------------------------------------------------------------------
+
+
+rm(list=ls(all=TRUE))
+
+##############################
+# Section 1 ##################
+##############################
+
+# Packages and function
+
+library(tidyverse)
+library(psych)
+library(plyr)
+library(Rfast)
+library(caret)
+library(dplyr)
+library(survival)
+library(furniture)
+library(boot)
+library(table1)
+
+here::here()
+
