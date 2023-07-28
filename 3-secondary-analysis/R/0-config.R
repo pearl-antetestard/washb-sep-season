71e1a8c8e21ea509a3069483cdf76fe9b762e7bb
pearl-antetestard <pearl-anne.ante@lecnam.net>
2023-07-28 16:46:09 -0700

    Codes for the spatial analysis

---
+++ 3-secondary-analysis/R/0-config.R
@@ -0,0 +1,21 @@
+
+#-------------------------------------------------------------------------------
+# @Organization - Proctor Foundation - UCSF
+# @Project - WASHB-SEP-Season 
+# @Author - Pearl Ante-Testard, pearl.ante@ucsf.edu
+# @Description - Configuration for maps
+#-------------------------------------------------------------------------------
+
+library(haven)
+library(here)
+library(mgcv)
+library(tidymv)
+library(tidygam)
+library(sf) # Mapping tool
+library(tidyverse) # Syntax for data interaction
+library(viridis) # Color palette
+library(stars) # Read .tif file
+library(ggrepel) # Graph label position
+library(colorspace)
+
+
